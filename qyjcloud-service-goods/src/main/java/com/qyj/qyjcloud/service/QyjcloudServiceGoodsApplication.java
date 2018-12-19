1e6d008e5a04f6f578873c4358850632cbd58532
helloaija <shitongle163@126.com>
2018-12-19 16:18:22 +0800

    服务链路追踪

--- qyjcloud-service-goods/src/main/java/com/qyj/qyjcloud/service/QyjcloudServiceGoodsApplication.java
+++ qyjcloud-service-goods/src/main/java/com/qyj/qyjcloud/service/QyjcloudServiceGoodsApplication.java
@@ -1,9 +1,15 @@
 package com.qyj.qyjcloud.service;
 
+import brave.sampler.Sampler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -13,4 +19,5 @@ public class QyjcloudServiceGoodsApplication {
     public static void main(String[] args) {
         SpringApplication.run(QyjcloudServiceGoodsApplication.class, args);
     }
+
 }
